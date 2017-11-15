2ee434d4918d12ea89ef964f906642d1b395a679
Derek Hendrickx <derek@hendrickx.me>
2017-11-15 17:10:10 -0500

    Create a tracker module

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,11 @@
 extern crate hyper;
-extern crate futures;
+use hyper::server::{Http};
 
-use futures::future::Future;
-
-use hyper::header::ContentLength;
-use hyper::server::{Http, Request, Response, Service};
-use hyper::{Method, StatusCode};
-
+mod tracker;
 mod announce;
-
-struct Tracker;
-
-const PHRASE: &'static str = "Hello, World!";
-
-impl Service for Tracker {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
-
-    fn call(&self, req: Request) -> Self::Future {
-        println!("Method: {:}", req.method());
-        println!("Path: {:}", req.path());
-
-        match (req.method(), req.path()) {
-            (&Method::Get, "/announce") => announce::Announce::announce(req.query()),
-            (&Method::Get, "/scrape") => println!("Scrape"),
-            _ => println!("{:}", StatusCode::NotFound),
-        }
-
-        match req.query() {
-            Some(str) => println!("Query: {:}", str),
-            None => println!("Query: None"),
-        }
-
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_body(PHRASE)
-        ))
-    }
-}
 
 fn main() {
     let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(Tracker)).unwrap();
+    let server = Http::new().bind(&addr, || Ok(tracker::Tracker)).unwrap();
     server.run().unwrap();
 }
---
+++ src/tracker.rs
@@ -0,0 +1,35 @@
+extern crate hyper;
+extern crate futures;
+
+use self::futures::future::Future;
+
+use hyper::header::ContentLength;
+use hyper::server::{Request, Response, Service};
+use hyper::{Method, StatusCode};
+
+use announce;
+
+const PHRASE: &'static str = "Hello, World!";
+
+pub struct Tracker;
+
+impl Service for Tracker {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        match (req.method(), req.path()) {
+            (&Method::Get, "/announce") => announce::Announce::announce(req.query()),
+            (&Method::Get, "/scrape") => println!("Scrape"),
+            _ => println!("{:}", StatusCode::NotFound),
+        }
+
+        Box::new(futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(PHRASE.len() as u64))
+                .with_body(PHRASE)
+        ))
+    }
+}
