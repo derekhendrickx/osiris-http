29ef89baee65c4b215679d59fef3e31558df55a4
Derek Hendrickx <derek@hendrickx.me>
2017-11-15 17:44:11 -0500

    Parse the querystring

--- src/announce.rs
+++ src/announce.rs
@@ -1,16 +1,23 @@
 extern crate hyper;
+extern crate qstring;
 
 use hyper::server::{Response};
+use self::qstring::QString;
 
 pub struct Announce;
 
 impl Announce {
 	pub fn announce(query: Option<&str>, response: &mut Response) {
 		println!("Announce");
+		let mut query_string = QString::from("");
+
 		match query {
-			Some(str) => println!("Query: {:}", str),
+			Some(str) => query_string = QString::from(str),
 			None => println!("Query: None"),
 		}
+
+		println!("Info hash: {:}", query_string["info_hash"]);
+
 		response.set_body("Announce to the tracker\nd5:filesd20:xxxxxxxxxxxxxxxxxxxxd8:completei2e10:downloadedi0e10:incompletei4e4:name12:xxxxxxxxxxxxee5:flagsd20:min_request_intervali3600eee");
 	}
 }
