1f2daac8b7e3248293b0ded6c47956159638c2cd
Derek Hendrickx <derek@hendrickx.me>
2017-11-20 17:51:39 -0500

    Update tracker port

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod announce;
 mod scrape;
 
 fn main() {
-    let addr = "127.0.0.1:3000".parse().unwrap();
+    let addr = "127.0.0.1:6969".parse().unwrap();
     let server = Http::new().bind(&addr, || Ok(tracker::Tracker)).unwrap();
     server.run().unwrap();
 }
