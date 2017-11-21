6310b5d134033f200ea8f31a10c1a961f034fafa
Derek Hendrickx <derek@hendrickx.me>
2017-11-20 22:19:26 -0500

    Allow to run the tracker on the local ip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate bip_bencode;
 extern crate hyper;
+extern crate local_ip;
 
+use std::net::{Ipv4Addr};
 use hyper::server::{Http};
 
 mod tracker;
@@ -9,7 +11,10 @@ mod announce;
 mod scrape;
 
 fn main() {
-    let addr = "127.0.0.1:6969".parse().unwrap();
+    // let ip = local_ip::get().unwrap();
+    let ip = Ipv4Addr::new(127, 0, 0, 1);
+    let addr = (ip.to_string() + ":6969").parse().unwrap();
     let server = Http::new().bind(&addr, || Ok(tracker::Tracker)).unwrap();
+    println!("Tracker running on {}...", addr);
     server.run().unwrap();
 }
