6129dc54a521a68fbe3b7c2bffd15a30c86b6726
Derek Hendrickx <derek@hendrickx.me>
2017-12-07 12:42:53 -0500

    Remove local ip cargo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate bip_bencode;
 extern crate hyper;
-extern crate local_ip;
 extern crate futures;
 #[macro_use]
 extern crate log;
@@ -18,8 +17,7 @@ mod peers;
 
 fn main() {
     env_logger::init().unwrap();
-    // let ip = local_ip::get().unwrap();
-    let ip = Ipv4Addr::new(127, 0, 0, 1);
+    let ip = Ipv4Addr::new(0, 0, 0, 0);
     let addr = (ip.to_string() + ":6969").parse().unwrap();
     let server = Http::new().bind(&addr, || {
         let routes = routes::Routes::new();
