1949fc1e61cce9eaae3f1de302f3a2ebb69acb82
Derek Hendrickx <derek@hendrickx.me>
2017-12-07 12:39:26 -0500

    Create a Peers struct

---
+++ src/peers.rs
@@ -0,0 +1,38 @@
+use std::net::{IpAddr};
+use std::collections::HashMap;
+
+pub struct Peer {
+    pub id: String,
+    port: u16,
+    ip: IpAddr,
+    uploaded: u32,
+    downloaded: u32,
+    left: u32,
+}
+
+impl Peer {
+    pub fn new(id: &str, port: u16, ip: IpAddr) -> Peer {
+        Peer {
+            id: String::from(id),
+            port,
+            ip,
+            uploaded: 0,
+            downloaded: 0,
+            left: 0,
+        }
+    }
+}
+
+struct File {
+    info_hash: String,
+    peers: HashMap<String, Peer>,
+}
+
+impl File {
+    fn new(info_hash: String) -> File {
+        File {
+            info_hash,
+            peers: HashMap::new(),
+        }
+    }
+}
